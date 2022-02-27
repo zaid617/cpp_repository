2b51ae31eff818efc592eb8231b22a68fb9e0df8
Zaid_ZiD <zid6177@gmail.com>
2022-02-27 14:43:28 +0500

    print user name using c++ program

---
+++ user_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ main(){
+    string name;
+    cout<<"Enter Your Name :";
+    cin>>name;
+    cout<<"Hello,"<<endl<< name<<", How are you.";
+return 0;
+}
